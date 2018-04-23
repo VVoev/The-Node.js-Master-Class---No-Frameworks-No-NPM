a375e1138bdaf77aa9c1bb2d90c88f4f16e5da6f
vladovoev <vladovoev@yahoo.co.uk>
2018-04-23 17:07:15 +0300

    added buffer for handling payload

--- Building_Restful_API/app/index.js
+++ Building_Restful_API/app/index.js
@@ -1,9 +1,9 @@
-
 //Primary file for the API
 
 //Dependancies
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
 //the server should respond to all requests with a string
 var server = http.createServer((req, res) => {
@@ -24,16 +24,28 @@ var server = http.createServer((req, res) => {
     //Get the headers as an object
     var headers = req.headers;
 
+    //Get the payload if any
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
 
-    //send the response
-    res.end('Hello Wolrd\n');
+    req.on('end', () => {
+        buffer += decoder.end();
 
-    //Log the request path
-    console.log('Request received on path ' + trimmedPath + 'with method ' + method + 'with queryString', queryStringObject);
-    console.log('Request received with this headers', headers);
+        //send the response
+        res.end('Hello Wolrd\n');
+
+        //Log the request path
+        console.log('Request received on path ' + trimmedPath + 'with method ' + method + 'with queryString', queryStringObject);
+        console.log('Request received with this payload', buffer);
+    })
+
 });
 
+var port = process.env.port || 3000;
 //start the server and have it listen on port 3000
-server.listen(3000, () => {
-    console.log(`The server is listening on port 3000 now`);
+server.listen(port, () => {
+    console.log(`The server is listening on port ${port} now`);
 })
