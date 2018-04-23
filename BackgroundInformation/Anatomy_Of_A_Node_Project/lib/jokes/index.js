9151af0f9792cef2e049f40ddbb72dc9588e3787
vladovoev <vladovoev@yahoo.co.uk>
2018-04-23 16:04:08 +0300

    Anatomy of a project

---
+++ BackgroundInformation/Anatomy_Of_A_Node_Project/lib/jokes/index.js
@@ -0,0 +1,22 @@
+// Dependencies
+var fs = require('fs');
+
+// App object
+var jokes = {};
+
+// Get all the jokes and return them to the user
+jokes.allJokes = function () {
+
+    // Read the text file containing the jokes
+    var fileContents = fs.readFileSync(__dirname + '/jokes.txt', 'utf8');
+
+    // Turn the string into an array 
+    var arrayOfJokes = fileContents.split(/\r?\n/);
+
+    // Return the array
+    return arrayOfJokes;
+
+};
+
+// Export the library
+module.exports = jokes;
