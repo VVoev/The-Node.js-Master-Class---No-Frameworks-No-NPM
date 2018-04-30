b79e69a88c530e7e6f9ad847a7673ecee90e5950
vladovoev <vladovoev@yahoo.co.uk>
2018-04-30 11:19:44 +0300

    edit a check

--- Building_Restful_API/app/lib/handlers.js
+++ Building_Restful_API/app/lib/handlers.js
@@ -259,6 +259,36 @@ handlers.checksList = (data, callback) => {
   } else {
     callback(405, undefined, 'html');
   }
+
+}
+//edit a check
+handlers.checksEdit = (data, callback) => {
+  // Reject any request that isn't a GET
+  if (data.method == 'get') {
+    // Prepare data for interpolation
+    var templateData = {
+      'head.title': 'Check Details',
+      'body.class': 'checksEdit'
+    };
+    // Read in a template as a string
+    helpers.getTemplate('checksEdit', templateData, (err, str) => {
+      if (!err && str) {
+        // Add the universal header and footer
+        helpers.addUniversalTemplates(str, templateData, (err, str) => {
+          if (!err && str) {
+            // Return that page as HTML
+            callback(200, str, 'html');
+          } else {
+            callback(500, undefined, 'html');
+          }
+        });
+      } else {
+        callback(500, undefined, 'html');
+      }
+    });
+  } else {
+    callback(405, undefined, 'html');
+  }
 }
 
 // Favicon
