c7b7bed3e940daebc996ed75e4142b962b4c788a
vladovoev <vladovoev@yahoo.co.uk>
2018-04-30 11:13:18 +0300

    Create a check

--- Building_Restful_API/app/lib/handlers.js
+++ Building_Restful_API/app/lib/handlers.js
@@ -231,6 +231,36 @@ handlers.checksCreate = (data, callback) => {
   }
 }
 
+//dashboard view all checks
+handlers.checksList = (data, callback) => {
+  // Reject any request that isn't a GET
+  if (data.method == 'get') {
+    // Prepare data for interpolation
+    var templateData = {
+      'head.title': 'Dashboard',
+      'body.class': 'checksList'
+    };
+    // Read in a template as a string
+    helpers.getTemplate('checksList', templateData, (err, str) => {
+      if (!err && str) {
+        // Add the universal header and footer
+        helpers.addUniversalTemplates(str, templateData, (err, str) => {
+          if (!err && str) {
+            // Return that page as HTML
+            callback(200, str, 'html');
+          } else {
+            callback(500, undefined, 'html');
+          }
+        });
+      } else {
+        callback(500, undefined, 'html');
+      }
+    });
+  } else {
+    callback(405, undefined, 'html');
+  }
+}
+
 // Favicon
 handlers.favicon = function (data, callback) {
   // Reject any request that isn't a GET
